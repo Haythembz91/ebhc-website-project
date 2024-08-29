f1a2ff54ea25f3b8f9e4d95f27feb22bfbdf7c39
Haythem91 <haylive8@gmail.com>
2024-08-29 15:42:19 +0100

    First commit

---
+++ src/app/components/Header.js
@@ -0,0 +1,22 @@
+
+
+const Header = ()=>{
+    return(
+        <header>
+            <div>
+                Logo
+            </div>
+            <nav>
+                <ul>
+                    <li>Accueil</li>
+                    <li>A propos</li>
+                    <li>Nos services</li>
+                    <li>Gallery</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
