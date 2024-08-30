a696b31105ba500157601ce24b9d8b0f5d934ee7
Haythem91 <haylive8@gmail.com>
2024-08-30 22:24:01 +0100

    First commit

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -3,12 +3,12 @@
 const Header = ()=>{
 
     return(
-        <header>
+        <header className={"container-xxl navbar bg-warning bg-opacity-25"}>
             <div>
                 Logo
             </div>
-            <nav>
-                <ul>
+            <nav className={"navbar"}>
+                <ul className={"navbar-nav flex-row list-unstyled"}>
                     <li>Accueil</li>
                     <li>A propos</li>
                     <li>Nos services</li>
