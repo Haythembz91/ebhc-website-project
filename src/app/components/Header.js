aa0a02f1e629d40e5f8810fb398d650e7a6032ee
Haythem91 <haylive8@gmail.com>
2024-09-01 14:51:37 +0100

    Created Slider.js

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,20 +1,42 @@
-
-
+'use client'
+import {useEffect} from "react";
+import logo from '../../../public/assets/logo_ebhc.png'
 const Header = ()=>{
 
+    useEffect(() => {
+        require("bootstrap/dist/js/bootstrap.bundle.min.js");
+    }, []);
+
     return(
-        <header className={"container-xxl navbar bg-warning bg-opacity-25"}>
-            <div>
-                Logo
-            </div>
-            <nav className={"navbar"}>
-                <ul className={"navbar-nav flex-row list-unstyled"}>
-                    <li className={'list-item'}>Accueil</li>
-                    <li className={'list-item'}>A propos</li>
-                    <li className={'list-item'}>Nos services</li>
-                    <li className={'list-item'}>Gallery</li>
-                    <li className={'list-item'}>Contact</li>
-                </ul>
+        <header className={"container-fluid bg-warning bg-opacity-25 sticky-top"}>
+            <nav className={"navbar navbar-expand-md justify-content-between"}>
+                <div className={"navbar-brand col-1"}>
+                    <img src={logo.src} className={"img-fluid"} alt={'logo ebhc'}/>
+                </div>
+                <div className={"d-flex flex-column"}>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <ul className={"navbar-nav collapse navbar-collapse"} id={"navbarSupportedContent"}>
+                        <li className={'list-item px-2'}>
+                            <a className={"nav-link py-2 px-0"}>HOME</a>
+                        </li>
+                        <li className={'list-item px-2'}>
+                            <a className={"nav-link py-2 px-0"}>ABOUT</a>
+                        </li>
+                        <li className={'list-item px-2'}>
+                            <a className={"nav-link py-2 px-0"}>SERVICES</a>
+                        </li>
+                        <li className={'list-item px-2'}>
+                            <a className={"nav-link py-2 px-0"}>PROJECTS</a>
+                        </li>
+                        <li className={'list-item px-2'}>
+                            <a className={"nav-link py-2 px-0"}>CONTACT US</a>
+                        </li>
+                    </ul>
+                </div>
             </nav>
         </header>
     )
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
-import Header from './components/Header.js'
+
+import Header from "@/app/components/Header.js";
+import Slider from "@/app/components/Slider.js";
 
 const Home = ()=>{
   return (
       <div>
         <Header></Header>
+        <Slider></Slider>
       </div>
   )
 }
